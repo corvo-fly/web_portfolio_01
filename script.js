168d4c7fa830ef902e3670c64c7bd9c13210691f
Jophet Montelibano <ravenawakened0413@gmail.com>
2026-04-18 22:50:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ const navLinks = document.getElementById("navLinks");
 hamburger.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+const sections = document.querySelectorAll("Section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const id = entry.target.getAttribute("id");
+
+            navLinks.forEach(link => {
+                link.classList.remove("active");
+                if (link.getAttribute("href") === "#" + id) {
+                    link.classList.add("active");
+                }
+            });
+        }
+    });
+}, {
+    threshold: 0.6
+});
+
+sections.forEach(section => {
+    observer.observe(section);
+});
