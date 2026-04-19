54ff9be8e3b9c756feff918df9bc4be5c16f09f0
Jophet Montelibano <ravenawakened0413@gmail.com>
2026-04-19 13:12:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("navLinks");
+const navMenu = document.getElementById("navLinks");
 
 hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+    navMenu.classList.toggle("active");
 });
 
-const sections = document.querySelectorAll("Section");
-const navLinks = document.querySelectorAll(".nav-links a");
+const sections = document.querySelectorAll("section");
+const navItems = document.querySelectorAll(".nav-links a");
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             const id = entry.target.getAttribute("id");
 
-            navLinks.forEach(link => {
+            navItems.forEach(link => {
                 link.classList.remove("active");
+
                 if (link.getAttribute("href") === "#" + id) {
                     link.classList.add("active");
                 }
@@ -25,6 +26,4 @@ const observer = new IntersectionObserver((entries) => {
     threshold: 0.6
 });
 
-sections.forEach(section => {
-    observer.observe(section);
-});
+sections.forEach(section => observer.observe(section));
